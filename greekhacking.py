5ef4c3ade57788bd95b66c28c80cd78abe95df28
MataGreek <89479885+MataGreek@users.noreply.github.com>
2021-09-30 02:32:04 +0300

    Add files via upload

--- greekhacking.py
+++ greekhacking.py
@@ -96,7 +96,7 @@ def check_updates():
 
         print(Fore.GREEN + "")
 
-        with open('./core/version.txt', 'w+') as vf:
+        with open('./core/version.txt', 'w+' + 'r') as vf:
 
             currentVersion = vf.read().strip()
 
