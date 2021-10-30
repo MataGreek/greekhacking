f6fb755b8a4f572e930d7c934c2c5e995526fcfc
MataGreek <89479885+MataGreek@users.noreply.github.com>
2021-10-30 19:48:16 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@ import time
 import os
 import sys
 from sys import platform
+
+yes_choice = ['Yes', 'y', 'Y', 'yes', 'YES']
+no_choice = ['No', 'n', 'no', 'NO', 'N']
+
 print(" Installing requirements....")
 time.sleep(2)
 
@@ -14,14 +18,17 @@ if sys.platform == "linux" or platform == "linux2":
 if sys.platform == "win32":
     cmd2 = 'curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py'
     cmd3 = 'python get-pip.py'
-    cmd4 = 'pip install requirements.txt'
+    cmd4 = 'pip install -r requirements.txt'
     os.system(cmd2)
     os.system(cmd3)
     os.system(cmd4)
 
-question = input("Do you want to open the program right now? (Y/n)  :    ")
-if question == 'Yes' or 'yes' or 'YES' or 'y' or 'Y':
+print("")
+print("")
+print("")
+question = input(" You are ready! Do you want to open the program right now? (Y/n):  ")
+
+if question in yes_choice:
     import greekhacking
-    pass
 else:
     quit()
